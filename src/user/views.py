da8da224242387d30fc1b16337eda1600d77fb96
Retrontology <retrontology@hotmail.com>
2022-10-01 20:15:56 -0500

    user not found

--- src/user/views.py
+++ src/user/views.py
@@ -15,23 +15,27 @@ def index(request):
 
 def user(request, username):
     username = username.lower()
-    template = loader.get_template('user/user.html')
     limit = int(request.GET.get('limit', DEFAULT_LIMIT))
     page = int(request.GET.get('page', 0))
     page_count = get_page_count(
         username=username,
         limit=limit
     )
-    messages = get_user_messages(
-        username=username,
-        limit=limit,
-        page=page
-    )
-    context = {
-        'username': username,
-        'messages': messages,
-        'page': page,
-        'limit': limit,
-        'last_page': page_count-1
-    }
-    return HttpResponse(template.render(context, request))
+    if page_count == 0:
+        template = loader.get_template('user/notfound.html')
+        return HttpResponse(template.render({'username': username}, request), status=404)
+    else:
+        template = loader.get_template('user/user.html')
+        messages = get_user_messages(
+            username=username,
+            limit=limit,
+            page=page
+        )
+        context = {
+            'username': username,
+            'messages': messages,
+            'page': page,
+            'limit': limit,
+            'last_page': page_count-1
+        }
+        return HttpResponse(template.render(context, request))
